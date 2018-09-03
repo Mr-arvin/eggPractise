f5c08d4ee71fe4386d3f1749ba7a6528e2b591f0
Mr-arvin <arvin1234567890@foxmail.com>
2018-09-03 23:50:59 +0800

    简单版MVC 架构

--- practise01/app/controller/home.js
+++ practise01/app/controller/home.js
@@ -7,6 +7,24 @@ class HomeController extends Controller {
     async index() {
         this.ctx.body = 'Hello world, Hello egg!';
     }
+    async about() {
+        this.ctx.body = "这个是关于";
+    }
+    async get() {
+        this.ctx.body = {
+            url: this.ctx.url,
+            method: this.ctx.method,
+            query: this.ctx.query
+        };
+    }
+    async getId() {
+        this.ctx.body = {
+            id: this.ctx.params.id,
+            url: this.ctx.url,
+            method: this.ctx.method,
+            query: this.ctx.query
+        };
+    }
 }
 
 module.exports = HomeController;
--- practise01/app/router.js
+++ practise01/app/router.js
@@ -1,5 +1,8 @@
 //配置路由映射 写要导出路由模块
 module.exports = app => {
     const { router, controller } = app;
-    router.get('/',controller.home.index);
+    router.get("/",controller.home.index);
+    router.get("/about", controller.home.about);
+    router.get("/get", controller.home.get);
+    router.get("/getid/:id", controller.home.getId);
 }
